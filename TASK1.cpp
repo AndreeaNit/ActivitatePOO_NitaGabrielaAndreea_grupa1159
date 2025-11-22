34dbefd7164c9eb4376e7c2b938aaa9fc177455c
Andreea Niță <nitaandreeaan82@gmail.com>
2025-11-22 22:52:20 +0200

    22.11.2025

--- TASK1.cpp
+++ TASK1.cpp
@@ -1,9 +1,39 @@
 #include<iostream>
 
 using namespace std;
+
+
+struct lego
+{
+	char* nume;
+	int cod;
+	float pret;
+};
+
+void afisareLego(lego g)
+{
+	cout << endl << "numele jocului este:" << g.nume << "si are codul" << g.cod << endl;
+
+}
+
+
+
+
 int main()
 {
 
 	cout << "Salutare";
+
+	cout << "primul task";
+
+	afisareLego()
+
 	return 0;
+
+
+
+
+
+
+
 }
